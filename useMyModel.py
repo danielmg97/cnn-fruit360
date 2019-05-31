439f5f7ec3f624b035b0d4c5dbfd001903431cb8
danielmg97 <danielmateusgoncalves@gmail.com>
2019-05-31 20:35:06 +0100

    Final version classifying all the fruits

--- useMyModel.py
+++ useMyModel.py
@@ -5,18 +5,18 @@ import numpy
 CATEGORIES = ["Apple Braeburn","Apple Crimson Snow","Apple Golden 1","Apple Golden 2","Apple Golden 3","Apple Granny Smith","Apple Pink Lady","Apple Red 1","Apple Red 2","Apple Red 3","Apple Red Delicious","Apple Red Yellow 1","Apple Red Yellow 2","Apricot","Avocado","Avocado ripe","Banana","Banana Lady Finger","Banana Red","Cactus fruit","Cantaloupe 1","Cantaloupe 2","Carambula","Cherry 1","Cherry 2","Cherry Rainier","Cherry Wax Black","Cherry Wax Red","Cherry Wax Yellow","Chestnut","Clementine","Cocos","Dates","Granadilla","Grape Blue","Grape Pink","Grape White","Grape White 2","Grape White 3","Grape White 4","Grapefruit Pink","Grapefruit White","Guava","Hazelnut","Huckleberry","Kaki","Kiwi","Kohlrabi","Kumquats","Lemon","Lemon Meyer","Limes","Lychee","Mandarine","Mango","Mangostan","Maracuja","Melon Piel de Sapo","Mulberry","Nectarine","Orange","Papaya","Passion Fruit","Peach","Peach 2","Peach Flat","Pear","Pear Abate","Pear Kaiser","Pear Monster","Pear Red","Pear Williams","Pepino","Pepper Green","Pepper Red","Pepper Yellow","Physalis","Physalis with Husk","Pineapple","Pineapple Mini","Pitahaya Red","Plum","Plum 2","Plum 3","Pomegranate","Pomelo Sweetie","Quince","Rambutan","Raspberry","Redcurrant","Salak","Strawberry","Strawberry Wedge","Tamarillo","Tangelo","Tomato 1","Tomato 2","Tomato 3","Tomato 4","Tomato Cherry Red","Tomato Maroon","Tomato Yellow","Walnut"]
 
 #a comment
-CATEGORIES = CATEGORIES[0:40]
+CATEGORIES = CATEGORIES[0:103]
 
 
 def prepare(filepath):
     IMG_SIZE = 100  # 50 in txt-based
     img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
     new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)/255.0
 
 
 model = tf.keras.models.load_model("64x3-CNN.model")
 
-prediction = model.predict([prepare('fruits-360/test-multiple_fruits/apple.jpg')])
+prediction = model.predict([prepare('fruits-360/Test/Apple Pink Lady/3_100.jpg')])
 print(prediction)  # will be a list in a list.
 print(CATEGORIES[numpy.where(prediction[0] == numpy.amax(prediction[0]))[0][0]])
